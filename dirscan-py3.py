a0f9dd77b795bedcaac1e5a9718c38735e11d204
Jaqen <591875624@qq.com>
2018-11-17 18:46:19 +0800

    增加代理池功能，但是由于代理ip质量太差影响扫描结果，待完善

--- dirscan-py3.py
+++ dirscan-py3.py
@@ -9,6 +9,7 @@ import urllib
 import threading
 import logging
 import lib.requests as requests
+from lib.get_proxy import proxy_list,check_proxy
 import random
 from lib.termcolor import colored
 import queue
@@ -34,10 +35,12 @@ config = {'lock': threading.Lock(),
     "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
     "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52", ],
     'customize_flag': 'n', #自定义字典标识位
-    '404': [ "Default.aspx","<title>404</title>", "<title>Error</title>", "<h3>TRACE</h3>", "This page can't be displayed", "HR noShade SIZE", "/error", "HTTP404", "404.aspx", "404 Not Found", "404.css", "404.png", "Your request is blocked"],  # 自定义404
+    '404': [ "Default.aspx","<title>404</title>", "<title>Error</title>", "<h3>TRACE</h3>", "This page can't be displayed", "HR noShade SIZE", "/error", "HTTP404", "404.aspx", "404 Not Found", "404.css", "404.png", "Your request is blocked", "mailto:anquan@jd.com"],  # 自定义404
     'firewall': ["safedog", "365cyd"], #防护设备
     'firewall-flag':False, #防护设备阻断标志
-    'result_list': []
+    'result_list': [],
+    'proxy_list': [],
+    'proxy_flag':False #是否使用代理池
 }
 
 
@@ -51,10 +54,16 @@ class my_thread(threading.Thread):
         while not config['workQueue'].empty():
             payload = config['workQueue'].get(True).lstrip('/')
             url = "%s/%s" % (self.domain.rstrip('/'), payload)
+            kwargs = {"timeout":config['timeout'], "headers":{"User-Agent": random.choice(
+                    config['USER_AGENTS'])}, "allow_redirects":config['allow_redirects'], "verify":False}
+            if config['proxy_flag']:
+                if len(config['proxy_list']) > 10:
+                    kwargs["proxies"] = {"http": random.choice(config['proxy_list']), "https": random.choice(config['proxy_list'])}
+                else:
+                    output("get useful proxy too less,keep real IP!",'red')
             # print url
             try:
-                resp = requests.get(url, timeout=config['timeout'], headers={"User-Agent": random.choice(
-                    config['USER_AGENTS'])}, allow_redirects=config['allow_redirects'], proxies=proxies, verify=False)
+                resp = requests.get(url, **kwargs)
             except Exception as e:
                 logging.error(e)
                 continue
@@ -64,8 +73,8 @@ class my_thread(threading.Thread):
                     for i in config['firewall']:
                         if i in resp.text:
                             if config['firewall-flag'] == False:
+                                config['firewall-flag'] = True
                                 output("有防护设备阻断-%s"%i, 'red')
-                            config['firewall-flag'] = True
                             return
                     for flag in config['404']:  # 自定义404
                         if flag in resp.text:
@@ -86,11 +95,11 @@ def Queue_push(dic):
     with open(dic, 'r') as dic:
         for dir in dic.readlines():
             if dir.startswith('/'):
-                dir = dir.rstrip('\n')
+                dir = dir.rstrip('\r').rstrip('\n')
                 # print dir
                 config['workQueue'].put(dir)
             else:
-                dir = dir.rstrip('\n')
+                dir = dir.rstrip('\r').rstrip('\n')
                 dir = "/"+dir
                 config['workQueue'].put(dir)
 
@@ -139,22 +148,40 @@ def dirscan(domain):
         logging.error(e)
         output("%s connect fail!" % domain, 'red')
         return
+    output('Starting scan target:%s' % domain, 'green')
+    if res.status_code in [200, 301, 302, 401, 403, 404]:
+        output(domain+" "+str(res.status_code))
+        # 取头信息
+        if 'Server' in dict(res.headers):
+            output('Server='+str(res.headers['server']), 'white', attrs=['bold'])
+        if 'X-Powered-By' in dict(res.headers):
+            output('X-Powered-By='+str(res.headers['X-Powered-By']), 'white', attrs=['bold'])
+        # 获取代理池
+        if config['proxy_flag']:
+            if os.path.exists('proxy_list.txt'):
+                with open('proxy_list.txt','r') as proxy_file:
+                    proxy_list = proxy_file.readlines()
+                if len(proxy_list) > 10:
+                    for i in proxy_list:
+                        config['proxy_list'].append(i.rstrip('\n'))
+                    output("Start check cache proxy useful,will loss some time", 'green')
+                    config['proxy_list'] = check_proxy(domain,config['proxy_list'])
+                    output("Complete check cache proxy useful,will loss some time", 'green')
+                else:
+                    output("Start get proxy list,will loss some time", 'green')
+                    config['proxy_list'] = proxy_list(domain)
+                    output("Complete get proxy list", 'green')
+            else:
+                output("Start get proxy list,will loss some time", 'green')
+                config['proxy_list'] = proxy_list(domain)
+                output("Complete get proxy list", 'green')
+        # 开始线程
+        for i in range(config['NUM']):
+            threads.append(my_thread(domain))
+        [t.start() for t in threads]
+        [t.join() for t in threads]
     else:
-        output('Starting scan target:%s' % domain, 'green')
-        if res.status_code in [200, 301, 302, 401, 403, 404]:
-            output(domain+" "+str(res.status_code))
-            # 取头信息
-            if 'Server' in dict(res.headers):
-                output('Server='+str(res.headers['server']), 'white', attrs=['bold'])
-            if 'X-Powered-By' in dict(res.headers):
-                output('X-Powered-By='+str(res.headers['X-Powered-By']), 'white', attrs=['bold'])
-            # 开始线程
-            for i in range(config['NUM']):
-                threads.append(my_thread(domain))
-            [t.start() for t in threads]
-            [t.join() for t in threads]
-        else:
-            output(domain+" "+str(res.status_code))
+        output(domain+" "+str(res.status_code))
     output('Complete scan target:%s' % domain, 'green')
 
 
@@ -179,6 +206,9 @@ if __name__ == "__main__":
     else:
         print('\n[*] starting at %s\n' % time.strftime('%H:%M:%S', time.localtime(time.time())))
         load_dict()
+        # 代理ip质量太差，影响扫描结果
+        # if  input("Use proxy pool?[y/n]").lower() != 'n':
+        #     config['proxy_flag'] = True
         if ".ini" in sys.argv[1]:
             try:
                 with open(sys.argv[1], 'r') as HostList:
@@ -189,7 +219,7 @@ if __name__ == "__main__":
                         if config['workQueue'].qsize() == 0:
                             load_dict()
                         config['firewall-flag'] = False
-                        dirscan(i.rstrip('\n'))
+                        dirscan(i.rstrip('\r').rstrip('\n'))
             except IOError:
                 output("导入域名清单出错", 'red')
         else:
---
+++ lib/get_proxy.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+# coding:utf-8
+#__author__="Jaqen"
+import requests
+import re
+import threading
+import logging
+import queue
+from lxml import html
+
+good_proxy = []
+cache_proxy = []
+proxy_queue = queue.Queue(0)
+def get_proxy1():
+    url = "http://www.66ip.cn/nmtq.php?getnum=1000&isp=1&anonymoustype=0&start=&ports=&export=&ipaddress=&area=0&proxytype=2&api=66ip"
+    try:
+        resp = requests.get(url,headers={'User-Agent':"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:56.0) Gecko/20100101 Firefox/56.0",'Cookie':"Hm_lvt_1761fabf3c988e7f04bec51acd4073f4=1542093293; Hm_lpvt_1761fabf3c988e7f04bec51acd4073f4=1542093820; yd_cookie=8a8786e6-3418-47eda62ada4672418079baf0f74b8bedbf68; _ydclearance=aebbe4fb0d9bff39f8066feb-d726-4472-9e42-5de7d83d1136-1542452439"})
+    except Exception as e:
+        logging.error(e)
+        return
+    if resp is not None and resp.status_code == 200:
+        result = re.findall('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{2,6}',resp.text)
+        for proxy in result:
+            proxy_queue.put(proxy)
+def get_proxy2():
+    for i in range(1,10):
+        url = 'https://www.kuaidaili.com/free/inha/%s/'%i
+        try:
+            resp = requests.get(url)
+        except Exception as e:
+            logging.error(e)
+            continue
+        if resp is not None and resp.status_code == 200:
+            resp = html.fromstring(resp.text)
+            ip_list = resp.xpath("//td[@data-title='IP']/text()")
+            port_list = resp.xpath("//td[@data-title='PORT']/text()")
+            for i in range(len(ip_list)):
+                proxy = ip_list[i] + ":" + port_list[i]
+                proxy_queue.put(proxy)
+
+def _fecth(test_url):
+    kwargs = {
+        "headers": {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:56.0) Gecko/20100101 Firefox/56.0",
+        },
+        "timeout": 3
+    }
+    while not proxy_queue.empty():
+        print('.',end="")
+        proxy = proxy_queue.get()
+        cache_proxy.append(proxy)
+        kwargs["proxies"] = {"http": proxy, "https": proxy}
+        try:
+            resp = requests.get(test_url, **kwargs)
+        except:
+            continue
+        if resp.status_code == 200:
+            with threading.Lock():
+                good_proxy.append(proxy)
+def save_cache():
+    with open("proxy_list.txt",'w') as cache_file:
+        for i in cache_proxy:
+            cache_file.write(i+"\n")
+    
+def proxy_list(domain):
+    threads = []
+    get_proxy1()
+    get_proxy2()
+    if proxy_queue.empty():
+        return []
+    fecth_proxy = [threading.Thread(target=_fecth,args=(domain,)) for i in range(10)]
+    threads.extend(fecth_proxy)
+    [thread.start() for thread in threads]
+    [thread.join() for thread in threads]
+    save_cache()
+    return good_proxy
+
+def check_proxy(domain,proxy_list):
+    threads = []
+    for i in proxy_list:
+        proxy_queue.put(i)
+    if proxy_queue.empty():
+        return []
+    fecth_proxy = [threading.Thread(target=_fecth,args=(domain,)) for i in range(10)]
+    threads.extend(fecth_proxy)
+    [thread.start() for thread in threads]
+    [thread.join() for thread in threads]
+    save_cache()
+    return good_proxy
